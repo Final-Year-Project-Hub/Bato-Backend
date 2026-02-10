8a3f0f72ec4332c3c21d9710f1fc3f83bae7a01e
Kuber-Pathak <kuberpathak124@gmail.com>
2026-02-10 22:18:57 +0545

    updated route

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -109,7 +109,7 @@ router.get(
       res.cookie("refreshToken", refreshToken, cookieOptions);
 
       return res.redirect(
-        `${process.env.FRONTEND_URL}/?accessToken=${encodeURIComponent(
+        `${process.env.FRONTEND_URL}/api/session/set?accessToken=${encodeURIComponent(
           accessToken,
         )}&refreshToken=${encodeURIComponent(refreshToken)}`,
       );
